da9deb91832e6b65d433b416ae556e30c3a56212
Levien van Zon <levien@zonnetjes.net>
2018-10-04 17:39:55 +0200

    Add missing DVARS for device 2, add code to sent packmsg-data to server

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 ragel -s p1-parser.rl
 gcc -Wall -Os -g -o p1-test p1-parser.c p1-lib.c p1-test.c crc16.c
-gcc -std=gnu99 -Wall -Os -g -o p1-packmsg p1-parser.c p1-lib.c p1-packmsg.c crc16.c mpack.c
+gcc -std=gnu99 -Wall -Os -g -o p1-packmsg p1-parser.c p1-lib.c p1-packmsg.c crc16.c mpack.c net_ip.c
 
 # To check for memory issues:
 # valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./p1-packmsg
--- p1-packmsg.c
+++ p1-packmsg.c
@@ -2,6 +2,9 @@
 
 #include "p1-lib.h"
 
+// For TCP/IP functions
+#include "net_ip.h"
+
 // For Messagepack:
 #include "mpack.h"
 
@@ -14,22 +17,20 @@ int main (int argc, char **argv)
 	init_msglogger();
 	logger.loglevel = LL_VERBOSE;
 	
-	char *infile, *dumpfile;
+	char *infile, *server, *port;
 	
-	if (argc < 2) {
-		logmsg(LL_NORMAL, "Usage: %s <input file or device> [<output for telegrams with parse errors>]\n", argv[0]);
+	if (argc < 4) {
+		logmsg(LL_NORMAL, "Usage: %s <input file or device> <server> <port>\n", argv[0]);
 		exit(1);
 	}
 	
 	infile = argv[1];
-	dumpfile = NULL;
-	
-	if (argc >= 3)
-		dumpfile = argv[2];
+	server = argv[2];
+	port = argv[3];
 	
 	telegram_parser parser;
 	
-	telegram_parser_open(&parser, infile, 0, 0, dumpfile);
+	telegram_parser_open(&parser, infile, 0, 0, NULL);
 	telegram_parser_read(&parser);
 
 	// TODO: Exit on errors, time-outs, etc.
@@ -143,6 +144,16 @@ int main (int argc, char **argv)
 	mpack_finish_array(&writer);
 	mpack_finish_array(&writer);
 	
+	// Specify variables for device 2
+	
+	mpack_start_array(&writer, 3);
+	mpack_write_cstr(&writer, "DVARS");
+	mpack_write_u8(&writer, 2);
+	mpack_start_array(&writer, 1);
+	mpack_write_cstr(&writer, "gas_in");
+	mpack_finish_array(&writer);
+	mpack_finish_array(&writer);
+	
 	// Write data to output stream	
 	// We can either send the data manually after destroying the writer,
 	// or define a flush-function with mpack_writer_set_flush()  
@@ -155,12 +166,24 @@ int main (int argc, char **argv)
 		printf("Wrote %lu bytes of total msgpack data\n", size);
 	}
 	
+
 	// Dump messages to file
 
 	FILE *out = fopen("msgpack.dat", "w");
 	fwrite(mpackdata, size, 1, out);
 	fflush(out);
-	
+
+    // Open server socket
+    // TODO check for errors
+
+    int sockfd = socket_connect_tcp(server, port);
+
+    // Send messages to server
+    // TODO check for errors
+
+    int len = size;
+    int result = sendall(sockfd, mpackdata, &len);
+
 	double last_gas_count = 0;
 	
 	do {
@@ -220,12 +243,23 @@ int main (int argc, char **argv)
 	
 		fwrite(mpackdata, size, 1, out);
 		fflush(out);
+
+        // Send messages to server
+        
+        len = size;
+        result = sendall(sockfd, mpackdata, &len);
+        // TODO check for errors
 				
 	} while (parser.terminal);		// If we're connected to a serial device, keep reading, otherwise exit
 	
 	fclose(out);
+    close(sockfd);
 	telegram_parser_close(&parser);
 	
 	return 0;
 }
 
+/*
+
+
+*/
