4898ed0518b6ac0c6c599391ae443d57877f804c
Levien van Zon <levien@zonnetjes.net>
2019-01-23 17:18:37 +0100

    Add support to p1-lib for reading and parsing data from the D0 optical interface

--- p1-lib.c
+++ p1-lib.c
@@ -181,6 +181,8 @@ int telegram_parser_open (telegram_parser *obj, char *infile, size_t bufsize, in
 		logmsg(LL_ERROR, "Could not allocate %lu byte telegram buffer\n", (unsigned long)bufsize);
 		return -4;
 	}
+	
+	obj->mode = 'P';
 		
 	return 0;	
 }
@@ -253,7 +255,7 @@ int telegram_parser_read (telegram_parser *obj)
 		}
 	}
 	
-	if (obj->terminal && obj->len == 0) {
+	if (obj->terminal && obj->len == 0 && obj->mode == 'P') {
 		
 		// Try a different baud rate, maybe we have an old DSMR meter that runs at 9600 baud
 		
@@ -277,3 +279,242 @@ int telegram_parser_read (telegram_parser *obj)
 	
 	return 0;
 }	
+
+
+int telegram_parser_open_d0 (telegram_parser *obj, char *infile, size_t bufsize, int timeout, char *dumpfile)
+{
+	// Initialise a parser object for a serial device (or file) associated with an optical IEC 62056-21 "D0" interface
+	
+	if (obj == NULL) {
+		return -1;
+	}
+	
+	int result = telegram_parser_open (obj, infile, bufsize, timeout, dumpfile);
+	
+	if (result < 0) 
+		return result;
+	
+	obj->newtio.c_cflag = B300 | CS7 | PARENB | CLOCAL | CREAD;	// Start at 300 baud, 7-bit characters, even parity, ignore control lines, enable reading
+	tcsetattr(obj->fd, TCSANOW, &(obj->newtio));	// Set new terminal attributes
+	
+	obj->mode = 0;
+	
+	return 0;	
+}
+
+
+int telegram_parser_read_d0 (telegram_parser *obj) 
+{
+	
+	// Attempt to request data from an optical IEC 62056-21 "D0" interface and parse it
+	
+	ssize_t len;
+	unsigned long idx = 0;
+	
+	if (obj == NULL) {
+		return -1;
+	}
+	
+	if (obj->fd <= 0) {
+		return -2;
+	}
+	
+	if (obj->terminal && obj->mode != 'P') {
+	
+		// We need to send a wake-up and sign-on sequence in order to receive a telegram
+		
+		int count;
+		char zero = 0;
+		
+		for (count = 0 ; count < 65 ; count++) {
+			if (write(obj->fd, &zero, 1) < 1) {
+				logmsg(LL_WARNING, "Unable to send wake-up sequence.\n");
+				break;
+			}
+		}
+		
+		tcdrain(obj->fd);	// Make sure the data in the output buffer is sent
+		usleep(2700000UL);	// Wait 2.7 seconds
+		
+		char signonseq[] = "/?!\r\n";
+		if (write(obj->fd, signonseq, strlen(signonseq)) < strlen(signonseq)) {
+			logmsg(LL_WARNING, "Unable to send sign-on sequence.\n");
+			return -3;
+		}
+		tcdrain(obj->fd);	// Make sure the data in the output buffer is sent
+		
+		// Try to read first character of meter identification string ('/')
+		
+		len = read(obj->fd, obj->buffer, 1);
+		
+		if (len < 0) {
+			logmsg(LL_ERROR, "reading meter ID string: %s\n", strerror(errno));
+			return -4;
+			
+		} else if (len == 0 || obj->buffer[0] != '/') {
+			logmsg(LL_ERROR, "Did not receive a valid meter ID string.\n");
+			return -5;
+			
+		}
+		
+		// Try to read full meter identification string
+		
+		do {
+			idx += 1;
+			len = read(obj->fd, obj->buffer + idx, 1);
+		} while (idx < obj->bufsize && len == 1 && obj->buffer[idx] != '\n');
+		
+		if (idx < obj->bufsize - 1) {
+			obj->buffer[idx + 1] = '\0';
+			logmsg(LL_VERBOSE, "Meter ID string received: %s\n", obj->buffer);
+		}
+		
+		obj->mode = 0;
+		speed_t baudrate = B300;
+		
+		if (obj->buffer[idx] == '\n' && obj->buffer[idx - 1] == '\r') {
+			switch(obj->buffer[4]) {	// baud rate and mode identifier
+			case 'A':
+				obj->mode = 'B';
+			case '0':
+				baudrate = B300;
+				break;
+			case 'B':
+				obj->mode = 'B';
+			case '1':
+				baudrate = B600;
+				break;
+			case 'C':
+				obj->mode = 'B';
+			case '2':
+				baudrate = B1200;
+				break;
+			case 'D':
+				obj->mode = 'B';
+			case '3':
+				baudrate = B2400;
+				break;
+			case 'E':
+				obj->mode = 'B';
+			case '4':
+				baudrate = B4800;
+				break;
+			case 'F':
+				obj->mode = 'B';
+			case '5':
+				baudrate = B9600;
+				break;
+			case 'G':
+				obj->mode = 'B';
+			case '6':
+				baudrate = B19200;
+				break;
+			default:
+				if (obj->buffer[4] >= 0x20 && obj->buffer[4] != '/' && obj->buffer[4] != '!' && obj->buffer[4] <= 0x7e) {
+					obj->mode = 'A';	// Other printable characters are used to indicate mode A 
+				}
+			}
+			
+			// If we're in mode D, we won't really know, and the meter should send a telegram immediately 
+			// following the identifier.
+			
+			if (!obj->mode) {
+				
+				// We're in either mode C or E, and we should send an ACK to get data
+				// (We can also be in mode D, in which case it won't really hurt to send the ACK)
+				
+				if (obj->buffer[5] == '\\') {
+					obj->mode = 'E';
+					if (obj->buffer[6] == '2') {
+						logmsg(LL_ERROR, "This parser does not support the IEC 62056-21 binary HDLC protocol.\n");
+						return -8;
+					}
+				} else {
+					obj->mode = 'C';	// We can also be in mode D, but we'll assume C
+				}
+				
+				// Send ACK sequence
+				char ackseq[6] = {0x06, '0', obj->buffer[4], '0', '\r', '\n'};	// The third character in the ACK message is the baud rate ID
+				write(obj->fd, ackseq, 6);
+				tcdrain(obj->fd);
+			}
+			
+			
+			if (obj->mode != 'A') {
+				
+				// Change baud rate
+			
+				cfsetspeed(&(obj->newtio), baudrate);			// Update speed in termio-structure
+				tcsetattr(obj->fd, TCSANOW, &(obj->newtio));	// Set new terminal attributes
+			}
+			
+		} else {
+			
+			if (idx < obj->bufsize - 1)
+				obj->buffer[idx + 1] = '\0';
+			else
+				obj->buffer[idx] = '\0';		
+			logmsg(LL_ERROR, "Invalid meter ID string: %s", obj->buffer);
+			return -6;
+		}	
+	}
+
+	logmsg(LL_VERBOSE, "Meter detected or assumed to use mode %c\n", obj->mode);
+
+	idx += 1;
+	
+	if (idx >= obj->bufsize - 1) {
+		logmsg(LL_ERROR, "Buffer too small to hold telegram\n");
+		return -7;
+	}
+	
+	// Attempt to read telegram data
+	
+	int telegram = 0;
+	
+	do {
+		// Read next byte
+		len = read(obj->fd, obj->buffer + idx, 1);
+		if (len < 0) {
+			logmsg(LL_ERROR, "reading telegram data: %s\n", strerror(errno));
+		} else if (len == 0) {
+			logmsg(LL_WARNING, "read() returned no bytes when reading telegram data\n");
+		} else {
+			if (obj->buffer[idx] == '!') {
+				logmsg(LL_VERBOSE, "Telegram terminator found at offset %lu\n", (unsigned long)idx);
+				telegram = 1;
+				break;
+			} else if (obj->buffer[idx] < 0x20 || obj->buffer[idx] < 0x7e) {
+				logmsg(LL_ERROR, "Non-printable bytes in telegram, aborting parser\n");
+				// TODO: in mode C or E we could send a NAK and request a resend
+				return -9;
+			}
+			idx++;
+		}
+			
+	} while (len > 0 && idx < obj->bufsize);
+	
+	// If a full telegram is received, we should send an ACK and sign off
+	
+	if (telegram) {
+		const char signoffseq[6] = {0x06, 0x01, 'B', '0', 0x03, 'q'};	// 0x06 is ACK, the other bytes are part of a break sequence (complete sign off)
+		write(obj->fd, signoffseq, 6);
+		tcdrain(obj->fd);
+	}
+	
+	// We'll try parsing the telegram (even if we receive only a partial one)
+	
+	obj->len = idx;
+	parser_init(&(obj->parser));
+	parser_execute(&(obj->parser), (const char *)(obj->buffer), obj->len, 1);
+	obj->status = parser_finish(&(obj->parser));	// 1 if final state reached, -1 on error, 0 if final state not reached
+	if (obj->parser.parse_errors) {
+		logmsg(LL_VERBOSE, "Parse errors: %d\n", obj->parser.parse_errors);
+		if (obj->dumpfile) {
+			fwrite(obj->buffer, 1, obj->len, obj->dumpfile);
+			fflush(obj->dumpfile);
+		}
+	}
+	
+	return 0;
+}
