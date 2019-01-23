87245addf2129523fdcededa476b85fa74aa5e9a
Levien van Zon <levien@zonnetjes.net>
2019-01-23 17:19:02 +0100

    Add test program for reading D0

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ragel -s p1-parser.rl
-gcc -Wall -Os -g -o p1-test p1-parser.c p1-lib.c p1-test.c crc16.c
+gcc -Wall -Os -g -o p1-test-p1 p1-parser.c p1-lib.c p1-test.c crc16.c
+gcc -Wall -Os -g -o p1-test-d0 p1-parser.c p1-lib.c p1-test-d0.c crc16.c
 
---
+++ p1-test-d0.c
@@ -0,0 +1,40 @@
+#include "logmsg.h"
+
+#include "p1-lib.h"
+
+
+int main (int argc, char **argv)
+{
+	
+	init_msglogger();
+	logger.loglevel = LL_VERBOSE;
+	
+	char *infile, *dumpfile;
+	
+	if (argc < 2) {
+		logmsg(LL_NORMAL, "Usage: %s <input file or device> [<output for telegrams with parse errors>]\n", argv[0]);
+		exit(1);
+	}
+	
+	infile = argv[1];
+	dumpfile = NULL;
+	
+	if (argc >= 3)
+		dumpfile = argv[2];
+	
+	telegram_parser parser;
+	
+	telegram_parser_open_d0(&parser, infile, 0, 0, dumpfile);
+		
+	do {
+		
+		telegram_parser_read_d0(&parser);
+		// TODO: figure out how to handle errors, time-outs, etc.
+			
+	} while (parser.terminal);		// If we're connected to a serial device, keep reading, otherwise exit
+	
+	telegram_parser_close(&parser);
+	
+	return 0;
+}
+
